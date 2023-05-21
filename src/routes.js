c15a96cd55acf8aa09bef064e2500d71325d8a73
julubis <julubis@github.com>
2023-05-21 18:44:05 +0700

    add routes API endpoint

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,61 @@
-const routes = [];
+const routes = [
+  {
+    method: 'GET',
+    path: '/users',
+    handler: () => {},
+  },
+  {
+    method: 'POST',
+    path: '/users/signin',
+    handler: () => {},
+  },
+  {
+    method: 'POST',
+    path: '/users/signup',
+    handler: () => {},
+  },
+
+  {
+    method: 'GET',
+    path: '/articles',
+    handler: () => {},
+  },
+  {
+    method: 'GET',
+    path: '/articles/{id}',
+    handler: () => {},
+  },
+  {
+    method: 'POST',
+    path: '/articles',
+    handler: () => {},
+  },
+  {
+    method: 'PUT',
+    path: '/articles/{id}',
+    handler: () => {},
+  },
+  {
+    method: 'DELETE',
+    path: '/articles/{id}',
+    handler: () => {},
+  },
+  {
+    method: 'POST',
+    path: '/articles/{id}/like',
+    handler: () => {},
+  },
+  {
+    method: 'POST',
+    path: '/articles/{id}/comments',
+    handler: () => {},
+  },
+
+  {
+    method: 'GET',
+    path: '/images/{id}',
+    handler: () => {},
+  },
+];
 
 module.exports = routes;
